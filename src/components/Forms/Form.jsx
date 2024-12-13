e43c466539bc419178748e1259d1cbcae94f8127
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-12-13 12:30:22 +0530

    Store form open state in local storage and access it from there
    When the user would reload the page, the forms would reset to being closed. Now they go back to the state they were at when the user reloaded the page.

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -4,16 +4,35 @@ import { useEffect, useState } from "react";
 
 export function Form({ title, inputs, onSave, profile }) {
   const [isSaved, setSaved] = useState(false);
+  const [isOpen, setOpen] = useState(() => {
+    const storedOpenState = localStorage.getItem(`${title} isOpen`);
+
+    return storedOpenState !== null
+      ? JSON.parse(storedOpenState)
+      : title === "Personal Details";
+  });
 
   const toCamelCase = (s) => s.replace(/-./g, (x) => x[1].toUpperCase());
 
   useEffect(() => {
     const profileFields = inputs.map((input) => toCamelCase(input.id));
-    const hasRequiredProfileValues = profileFields.every(
+    const hasRequiredProfileValues = profileFields.some(
       (key) => profile && profile[key],
     );
     setSaved(hasRequiredProfileValues);
   }, [inputs, profile]);
+
+  useEffect(() => {
+    console.log(title, isOpen);
+
+    localStorage.setItem(`${title} isOpen`, isOpen);
+  }, [isOpen, title]);
+
+  function handleOpen(event) {
+    console.log(isOpen);
+    event.preventDefault();
+    setOpen((isOpen) => !isOpen);
+  }
 
   function handleSave(event) {
     event.preventDefault();
@@ -29,10 +48,13 @@ export function Form({ title, inputs, onSave, profile }) {
   return (
     <form onSubmit={handleSave}>
       <details
-        open={title === "Personal Details"}
+        open={isOpen}
         className="rounded-xl bg-white p-4 hover:bg-zinc-400 open:hover:bg-white dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:open:hover:bg-zinc-800"
       >
-        <summary className="cursor-pointer px-4 py-2 text-2xl font-bold">
+        <summary
+          onClick={handleOpen}
+          className="cursor-pointer px-4 py-2 text-2xl font-bold"
+        >
           {title}
         </summary>
         <fieldset disabled={isSaved} className="space-y-3">
